79a601dc8e5ca7ad3980a46b269d355407ab26f2
saydali123 <murodullayev.saydali0080@gmail.com>
2022-03-27 21:34:47 +0500

    Initial commit

---
+++ src/main/java/com/example/modul2_theme1_task2/Modul2Theme1Task2Application.java
@@ -0,0 +1,13 @@
+package com.example.modul2_theme1_task2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modul2Theme1Task2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Modul2Theme1Task2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/modul2_theme1_task2/Modul2Theme1Task2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modul2_theme1_task2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Modul2Theme1Task2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
